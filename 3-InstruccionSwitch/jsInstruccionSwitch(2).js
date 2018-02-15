4a9af8d5e0c27c78d355bf9f4f9786ca65670823
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-15 09:44:03 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -6,20 +6,10 @@ var mesDelAño = document.getElementById('mes').value;
 switch (mesDelAño)
 {
     case "Enero":
-        alert("Falta para el invierno.");
-        break;
     case "Febrero":
-        alert("Falta para el invierno.");
-        break;
     case "Marzo":
-        alert("Falta para el invierno.");
-        break;
-    case "Abril":
-        alert("Falta para el invierno.");
-        break;
+    case "Abril":   
     case "Mayo":
-        alert("Falta para el invierno.");
-        break
     case "Junio":
         alert("Falta para el invierno.");
         break;
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,22 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+	switch(mesDelAño)
+	{
+       case"Febrero":
+           alert("tiene 28 días");
+           break;
+       case "Abril":
+       case "Mayo":
+       case "Junio":
+       case "Septiembre":
+       case "Noviembre":
+            alert("tiene 30 días");
+            break;
+       default:
+            alert("tiene 31 días.");
+            break;              
+    }
 
 
 
