592bb7929b6105fd9b94f9b48c85b6d1f98c2895
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-15 12:17:13 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 10.");
-
-
+//rehacerlo!
+	var numero;
+	numero= parseInt(prompt("ingrese un número entre 0 y 10."));
+	while(!(numero>=0 && numero<=10))
+	{
+		numero=parseInt(prompt("ingrese nuevamente un número entre 0 y 10."));
+	}
+	alert("Correcto");
 }//FIN DE LA FUNCIÓN
