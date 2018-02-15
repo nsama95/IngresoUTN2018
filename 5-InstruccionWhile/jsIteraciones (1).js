1dec4d05a8b82b0dbd0cb5ec7dd91141e642ec49
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-15 10:41:37 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -2,7 +2,7 @@ function Mostrar()
 {
 var numero;
 numero =1;
-while (numero < 11);
+while (numero < 11)
 {
 	console.log(numero);
 	numero= numero+1
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+var numero;
+numero =10;
+while (numero > 0)
+{
+	console.log(numero);
+	numero= numero - 1;
+}
 
 
 }//FIN DE LA FUNCIÓN
