1da6652f7a5c793622fddda33cf57ef20efc2fb9
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-21 12:29:25 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,31 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var numero;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
-
-
+	var respuesta="si";
+	var contadorneg;
+	while( respuesta =="si")
+	{
+		numero= parseInt(prompt("Ingrese un numero."));
+		
+		if(numero>=0)
+		{
+			positivo= positivo + numero;
+		}
+		else 
+		{
+			negativo= negativo*numero;
+			contadorneg++;
+		}
+		respuesta= prompt("Desea continuar?");
+	}
 document.getElementById('suma').value=positivo;
+if(contadorneg == 0)
+		{
+			negativo= 0;
+		}
 document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
