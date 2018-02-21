4f57218dc9edb9c9204e3b6d03a06d6e54509ec0
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-20 21:55:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad= parseInt(document.getElementById("edad").value);
+if (edad >= "13" && edad <= "18")
+{
+    alert("Usted es adolecente");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,11 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad= parseInt(document.getElementById("edad").value);
+if (!(edad >= "13" && edad <= "18"))
+{
+    alert("Usted NO es adolecente");
+}
 
 }//FIN DE LA FUNCIÓN
