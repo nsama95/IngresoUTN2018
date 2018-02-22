ea4a40bb164097f99d90b6313fb836769bcc2258
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-02-22 12:27:12 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
-
-
-
+    for(numero=1;;numero++)
+    {
+        numero=parseInt(prompt("ingrese el número de repeticiones"));
+        if (numero%2==0)
+        {
+            numero++;
+        }
+    }
 
 }//FIN DE LA FUNCIÓN
