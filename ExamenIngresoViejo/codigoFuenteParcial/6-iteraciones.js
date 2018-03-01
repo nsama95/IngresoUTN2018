caaffcbeab9848f709bec4f241973e6e8f359c23
nsama95 <36465661+nsama95@users.noreply.github.com>
2018-03-01 10:47:35 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
@@ -1,6 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+	var importe;
+    var mayor;
+    var menor;
+    
+    for(i=1;i<=7;i++)
+    {
+     importe= parseFloat(prompt("Ingrese el importe: "));
+    while(importe>0)
+
 }
 
